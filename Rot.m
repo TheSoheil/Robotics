803fb266ff4ce36bea84b8e7c0a687a2cc2bcb99
TheSoheil <145009082+TheSoheil@users.noreply.github.com>
2024-06-11 20:54:11 +0330

    Add files via upload
    rotation transformation

---
+++ Rot.m
@@ -0,0 +1,14 @@
+function R=Rot(axis, angle)
+%Homogenous transformation for a rotation around "axis" for the amount of "angle"
+axis = upper(axis);
+angle = deg2rad(angle);
+if (axis == 'X')
+R=[1 0 0 0; 0 cos(angle) -sin(angle) 0; 0 sin(angle) cos(angle) 0; 0 0 0 1];
+end
+if (axis == 'Y')
+R=[cos(angle) 0 sin(angle) 0; 0 1 0 0; -sin(angle) 0 cos(angle) 0; 0 0 0 1];
+end
+if (axis =='Z')
+R=[cos(angle) -sin(angle) 0 0; sin(angle) cos(angle) 0 0; 0 0 1 0; 0 0 0 1];
+end
+end
